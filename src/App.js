2cd82387ec44d007c7d5fde6b46f67ffff6d7356
Lgk9583 <dunngen2@live.se>
2020-05-26 15:23:21 +0200

    Implemented saving movies to localstorage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import Home from  "./Home"
-import MoviePage from  "./MoviePage"
-import SavedMoviesPage from  "./SavedMoviesPage"
+import Home from "./Home"
+import MoviePage from "./MoviePage"
+import SavedMoviesPage from "./SavedMoviesPage"
 import Layout from "./layout"
 
-import {BrowserRouter,Route,Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function App() {
 
- 
+
 
   return (
-   <BrowserRouter>
+    <BrowserRouter>
 
-    <Switch>
+      <Switch>
 
-    <Route path="/" exact component={Home} />
-    <Route  path="/saved-movies" exact component={SavedMoviesPage} />
-    <Route  path="/movie/:id" exact component={MoviePage} />
+        <Route path="/" exact component={Home} />
+        <Route path="/saved-movies" exact component={SavedMoviesPage} />
+        <Route path="/movie/:id" exact component={MoviePage} />
 
-    <Route  path="/" render={ () =>
-        <Layout>
-          <h1 style={{marginTop:"20px", textAlign:"center"}}>404</h1>
-        </Layout>
-    } />
+        <Route path="/" render={() =>
+          <Layout>
+            <h1 style={{ marginTop: "20px", textAlign: "center" }}>404</h1>
+          </Layout>
+        } />
 
-    </Switch>
+      </Switch>
 
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/MoviePage.js
+++ src/MoviePage.js
@@ -1,41 +1,52 @@
-import React,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from "./layout";
 
-import { useRouteMatch,Link } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 
 
-const MoviePage = ()=>{
-    const match = useRouteMatch();
-    useEffect(()=>{
+const saveMovie = (id) => {
+  let jsonMovies = JSON.parse(localStorage.getItem("movies"));
+  if(Array.isArray(jsonMovies) === false)
+    jsonMovies = [];
+
+  if(jsonMovies.includes(id) === false) {
+    jsonMovies.push(id);
+    localStorage.setItem("movies", JSON.stringify(jsonMovies));
+  }
+}
+
+const MoviePage = () => {
+  const match = useRouteMatch();
+  const matchID = match.params.id;
+  useEffect(() => {
 
 
-        fetch("https://api.themoviedb.org/3/movie/" + match.params.id + "/videos?api_key=d1426caae4746a010983a3dbcf8acc35&language=en-US" )
-        .then(res => res.json())
-        .then(
-          (data) => {
-           
-
-            //
-            
-          },
-        
-          (error) => {
-          }
-        )
+    fetch("https://api.themoviedb.org/3/movie/" + matchID + "/videos?api_key=d1426caae4746a010983a3dbcf8acc35&language=en-US")
+      .then(res => res.json())
+      .then(
+        (data) => {
 
 
+          //
 
-    },[match.params.id])
+        },
 
-    return(
-        <Layout >
-          <div>{match.params.id}</div>
-          <div>Vi gör nytt API-anrop för att hämta mer info enskilde filmen här</div>
-          <div>plus gör det möjligt för användaren att spara filmen till localstorage</div>
+        (error) => {
+        }
+      )
 
 
-        </Layout>
-    )
+
+  }, [matchID])
+
+  return (
+    <Layout >
+      <div>{matchID}</div>
+      <div>Vi gör nytt API-anrop för att hämta mer info enskilde filmen här</div>
+      <div>plus gör det möjligt för användaren att spara filmen till localstorage</div>
+      <button onClick={() => saveMovie(matchID)}>Save</button>
+    </Layout>
+  )
 
 }
 
